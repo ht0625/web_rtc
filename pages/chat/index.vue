c3249d8ca25885f9ac011f6db7a71ef2e3f5e7b9
Tamura <67675234+ht0625@users.noreply.github.com>
2024-11-28 12:09:57 +0900

    JSONデータを通信するように修正

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -19,7 +19,8 @@ const messages = ref<string[]>([]);
 
 const sendMessageOffer = () => {
   if (webrtcStore.offerSendChannel && webrtcStore.offerSendChannel.readyState === 'open') {
-    webrtcStore.offerSendChannel.send(message.value);
+    const sendData = JSON.stringify({ type:'url', data:message.value });
+    webrtcStore.offerSendChannel.send(sendData);
     displayMessage(`送信: ${message.value}`);
     message.value = '';
   }else{
@@ -32,7 +33,8 @@ const sendMessageOffer = () => {
 
 const sendMessageAnswer = () => {
   if (webrtcStore.answerSendChannel && webrtcStore.answerSendChannel.readyState === 'open') {
-    webrtcStore.answerSendChannel.send(message.value);
+    const sendData = JSON.stringify({ type:'url', data:message.value });
+    webrtcStore.answerSendChannel.send(sendData);
     displayMessage(`送信: ${message.value}`);
     message.value = '';
   }else{
@@ -47,7 +49,12 @@ watch(
   () => webrtcStore.offerReceiveChannel,
   (channel) => {
     if (channel) {
-      channel.onmessage = (e) => displayMessage(`受信: ${e.data}`);
+    //   channel.onmessage = (e) => displayMessage(`受信: ${e.data}`);
+      channel.onmessage = (e) => {
+        const receivedData = JSON.parse(e.data);
+        console.log(receivedData)
+        displayMessage(`受信: ${receivedData.data}`)
+      };
       console.log('受信チャンネルが設定されました');
     }
   },
@@ -58,7 +65,12 @@ watch(
   () => webrtcStore.answerSendChannel,
   (channel) => {
     if (channel) {
-      channel.onmessage = (e) => displayMessage(`受信: ${e.data}`);
+    //   channel.onmessage = (e) => displayMessage(`受信: ${e.data}`);
+    channel.onmessage = (e) => {
+        const receivedData = JSON.parse(e.data);
+        console.log(receivedData)
+        displayMessage(`受信: ${receivedData.data}`)
+      };
       console.log('受信チャンネルが設定されました');
     }
   },
